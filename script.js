3e257d4ab5c1ed60e56bc2184e1d139284bfd6b4
LouSplendido <lourdes.splendido@gmail.com>
2023-09-14 19:54:53 -0300

    Agregando letras para alert

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let nombre = (prompt ("Ingrese su nombre"))
 
+while (!nombre || !/^[a-zA-Z]+$/.test(nombre)) {
+  nombre = prompt("Ingrese su nombre ");
+}
+
 alert ("Bienvenido/a " + nombre + " a nuestra página 'Vegan Style'")
+
