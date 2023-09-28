f0923ef326f4cf8bfe3211ba51062d81c690533f
LouSplendido <lourdes.splendido@gmail.com>
2023-09-28 20:58:13 -0300

    Formulario con validacion

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
-let nombre = (prompt ("Ingrese su nombre"))
+/*let nombre = (prompt ("Ingrese su nombre"))
 
 while (!nombre || !/^[a-zA-Z]+$/.test(nombre)) {
   nombre = prompt("Ingrese su nombre ");
 }
 
 alert ("Bienvenido/a " + nombre + " a nuestra página 'Vegan Style'")
+*/
 
+
+//Seccion "contactos". Formulario
+
+//Mensaje de error si el pass no cumple con ciertos requisitos
+window.onload = () => {
+  let formulario = document.querySelector("form");
+  let inputNombre = document.getElementById("nombre");
+  let inputApellido = document.getElementById("apellido")
+  //let inputEmail = document.getElementById("email")
+  let inputSubmit = document.getElementById("submit")
+
+inputNombre.addEventListener("keyup", () => {
+  let mensaje;
+  if(inputNombre.value.length < 3 ){
+      mensaje = "El nombre tiene que tener al menos 3 caracteres"
+      document.getElementById("mensajeNombre").innerText = mensaje;
+  }else{
+      document.getElementById("mensajeNombre").innerText = " "
+  }
+})
+
+inputApellido.addEventListener("keyup", () => {
+  let mensaje1;
+  if(inputApellido.value.length < 3 ){
+      mensaje1 = "El nombre tiene que tener al menos 3 caracteres"
+      document.getElementById("mensajeApellido").innerText = mensaje1;
+  }else{
+      document.getElementById("mensajeApellido").innerText = " "
+  }
+})
+
+
+}
