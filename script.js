f5ad1950f53a8118f06ba089b13b2cbbbfd48d22
LouSplendido <lourdes.splendido@gmail.com>
2023-09-28 21:04:01 -0300

    Validando el formulario parte 1

--- script.js
+++ script.js
@@ -5,4 +5,38 @@ while (!nombre || !/^[a-zA-Z]+$/.test(nombre)) {
 }
 
 alert ("Bienvenido/a " + nombre + " a nuestra página 'Vegan Style'")
+<<<<<<< HEAD
+*/
+//Seccion "contactos". Formulario
+
+//Mensaje de error si el nombre/apellido no cumple con ciertos requisitos
+window.onload = () => {
+  let formulario = document.querySelector("form");
+  let inputNombre = document.getElementById("nombre");
+  let inputApellido = document.getElementById("apellido")
+  //let inputEmail = document.getElementById("email")
+  let inputSubmit = document.getElementById("submit")
+
+inputNombre.addEventListener("keyup", () => {
+  let mensaje;
+  if(inputNombre.value.length < 3 ){
+      mensaje = "El nombre tiene que tener al menos 3 caracteres"
+      document.getElementById("mensajeNombre").innerText = mensaje;
+  }else{
+      document.getElementById("mensajeNombre").innerText = " "
+  }
+})
+
+inputApellido.addEventListener("keyup", () => {
+  let mensaje1;
+  if(inputApellido.value.length < 3 ){
+      mensaje1 = "El nombre tiene que tener al menos 3 caracteres"
+      document.getElementById("mensajeApellido").innerText = mensaje1;
+  }else{
+      document.getElementById("mensajeApellido").innerText = " "
+  }
+})
+
+
+}
 
